a33b1306dbee4a028755d68558116dbb127b6ccb
Dmitriy Truten <71496906+DmitryTruten@users.noreply.github.com>
2022-05-30 18:26:21 +0300

    Delete name_function.py

--- name_function.py
+++
@@ -1,7 +0,0 @@
-def get_formatted_name(first, last, middle=''):
-    """Generate formatted full name"""
-    if middle:
-        full = f"{first} {middle} {last}"
-    else:
-        full = f"{first} {last}"
-    return full.title()
